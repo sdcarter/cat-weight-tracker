bdb0fa9f147da71bdca88c263938a5481e9800c9
Steve Carter <carter.sd@gmail.com>
2025-05-23 09:30:08 +0200

    chore: remove redundant lint.sh script and ignore database backups

--- lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Running frontend linting..."
-docker-compose run --rm frontend npm run lint
-
-echo "Running backend linting..."
-docker-compose run --rm backend flake8
-
-echo "Linting complete!"

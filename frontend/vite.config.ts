590ca032fe37f76b05f2eeea1708b19d8ba0b0bf
Steve Carter <carter.sd@gmail.com>
2025-07-11 13:42:02 +0200

    fix: update Vite config to use sensible file names for output files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
